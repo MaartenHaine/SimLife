f2c4b2a8bf1d08dc751757259e6688f06ee8e025
MaartenHaine <maarten.haine@student.kuleuven.be>
2024-05-04 22:02:32 +0200

    Completed Simulation, neurons still need some work

--- src/main/java/sim/neuralnet/ActivationFunctionNeuron.java
+++ src/main/java/sim/neuralnet/ActivationFunctionNeuron.java
@@ -5,11 +5,21 @@ import java.util.ArrayList;
 import sim.entities.Prey;
 import util.Pair;
 
+/**
+ * @invar | getDependencies() != null
+ * @invar | getDependencies().stream().allMatch(p -> p != null)
+ * @invar | getDependencies().size() <= 7
+ */
 
 public abstract class ActivationFunctionNeuron implements Neuron
 {
 
 	/**
+	 * @representationObject
+	 * @representationObjects
+	 * @invar | dependencies != null
+	 * @invar | dependencies.stream().allMatch(p -> p != null)
+	 * @invar | dependencies.size() >= 0
 	 * Can be any >= 0 size
 	 */
     private ArrayList<Pair<Neuron, Integer>> dependencies;
@@ -18,18 +28,43 @@ public abstract class ActivationFunctionNeuron implements Neuron
     
     /**
      * The Neuron references are freely accessible by the client
+     * @creates | result
+     * @post | result != null
+     * @post | result.stream().allMatch(p -> p != null)
      */
     public ArrayList<Pair<Neuron, Integer>> getDependencies() {
-    	return null;
+    	ArrayList<Pair<Neuron, Integer>> res = new ArrayList<Pair<Neuron, Integer>>();
+    	for (Pair<Neuron, Integer> p : dependencies) {
+    		Pair<Neuron, Integer> aux = new Pair<Neuron, Integer>(p.getFirst(), p .getSecond());
+    		res.add(aux);
+    	}
+    	return res;
+
     }
     
-
+    /**
+     * @mutates | this
+     * @inspects | deps
+     * @pre | deps != null
+     * @pre | deps.stream().allMatch(p -> p != null)
+     * @pre | deps.size() <= 7
+     * @post | getDependencies().size() == deps.size()
+     * @post | equalDependencies(deps, getDependencies())
+     * @mutates_properties | getDependencies()
+     */
     public void setDependencies(ArrayList<Pair<Neuron, Integer>> deps) {
-
+        ArrayList<Pair<Neuron, Integer>> res = new ArrayList<Pair<Neuron, Integer>>();
+    	for (Pair<Neuron, Integer> p : deps) {
+    		res.add(new Pair<Neuron, Integer>(p.getFirst() , p.getSecond()));
+    	}
+    	dependencies = res;
     }
 
     /**
      * Initializes with getBias = 0 and getDependencies is empty
+     * 
+     * @post | getDependencies().size() == 0
+     * @post | getBias() == 0
      */
     public ActivationFunctionNeuron()
     {
@@ -37,11 +72,32 @@ public abstract class ActivationFunctionNeuron implements Neuron
         bias = 0;
     }
 
+    /**
+     * If the connection fails, do nothing and return false
+     *
+     * @mutates | this
+     * @pre | dependency != null
+     * @post | implies(old(getDependencies().size()) == 7, equalDependencies(old(getDependencies()), getDependencies()) && !result)
+     * @post | implies(old(getDependencies().size()) < 7, equalDependenciesWithAddedPair(old(getDependencies()), getDependencies(), dependency, weight) && result)
+     */
 
     public boolean connect(Neuron dependency, int weight)
     {
-        return true;
+    	if (dependencies.size() == 7)
+    	{
+    		return false;
+		}
+    	else
+    	{
+            var pair = new Pair<Neuron, Integer>(dependency, weight);
+            dependencies.add(pair);
+            return true;
+    	}
     }
+
+    /**
+     * @post | getBias() == bias
+     */
 
     public void setBias(int bias)
     {
--- src/main/java/sim/neuralnet/HunterSensor.java
+++ src/main/java/sim/neuralnet/HunterSensor.java
@@ -4,8 +4,17 @@ import sim.entities.Prey;
 
 public class HunterSensor extends BinarySensorNeuron {
 
+	/**
+	 * A HunterSensor detects if there is a hunter somewhere ahead of the prey. 
+	 * This logic is implemented in World.hasHunterInCone.
+	 * 
+	 * @pre | prey != null
+	 * @post | result == prey.getWorld().hasHunterInCone(prey.getPosition(), prey.getOrientation())
+	 */
 	@Override
 	public boolean detect(Prey prey) {
-		return false;
+
+		return prey.getWorld().hasHunterInCone(prey.getPosition(), prey.getOrientation());
+		
 	}
 }
