74d5d85d71cbbf65242ab3278a909f4142e6a4d3
MaartenHaine <maarten.haine@student.kuleuven.be>
2024-05-05 17:26:22 +0200

    edited functions in ActivationFunctionNeuron

--- src/main/java/sim/neuralnet/ActivationFunctionNeuron.java
+++ src/main/java/sim/neuralnet/ActivationFunctionNeuron.java
@@ -1,6 +1,9 @@
 package sim.neuralnet;
 
 import java.util.ArrayList;
+import java.util.stream.IntStream;
+import util.Pair;
+import static util.Logic.*;
 
 import sim.entities.Prey;
 import util.Pair;
@@ -10,22 +13,54 @@ public abstract class ActivationFunctionNeuron implements Neuron
 {
 
 	/**
-	 * Can be any >= 0 size
+	 * @representationObject
+	 * @representationObjects
+	 * @invar | dependencies != null
+	 * @invar | dependencies.stream().allMatch(p -> p != null)
+	 * @invar | dependencies.size() <= 5
 	 */
+
     private ArrayList<Pair<Neuron, Integer>> dependencies;
 
     private int bias;
     
     /**
      * The Neuron references are freely accessible by the client
+     *
+     * @creates | result
+     * @post | result != null
+     * @post | result.stream().allMatch(p -> p != null)
      */
+
     public ArrayList<Pair<Neuron, Integer>> getDependencies() {
-    	return null;
+    	ArrayList<Pair<Neuron, Integer>> res = new ArrayList<Pair<Neuron, Integer>>();
+    	for (Pair<Neuron, Integer> p : dependencies) {
+    		Pair<Neuron, Integer> aux = new Pair<Neuron, Integer>(p.getFirst(), p .getSecond());
+    		res.add(aux);
+    	}
+    	return res;
+
     }
     
+    /**
+     * pre: deps has correct length
+     * The Neuron references are stored as is.
+     *
+     * @mutates | this
+     * @inspects | deps
+     * @pre | deps != null
+     * @pre | deps.stream().allMatch(p -> p != null)
+     * @pre | deps.size() <= 5
+     * @post | getDependencies().size() == deps.size()
+     * @mutates_properties | getDependencies()
+     */
 
     public void setDependencies(ArrayList<Pair<Neuron, Integer>> deps) {
-
+    	ArrayList<Pair<Neuron, Integer>> res = new ArrayList<Pair<Neuron, Integer>>();
+    	for (Pair<Neuron, Integer> p : deps) {
+    		res.add(new Pair<Neuron, Integer>(p.getFirst() , p.getSecond()));
+    	}
+    	dependencies = res;
     }
 
     /**
@@ -37,10 +72,29 @@ public abstract class ActivationFunctionNeuron implements Neuron
         bias = 0;
     }
 
+    /**
+     * If the connection fails, do nothing and return false
+     *
+     * @mutates | this
+     * @pre | dependency != null
+     * @post | implies(old(getDependencies().size()) == 5, result == false)
+     * @post | implies(old(getDependencies().size()) < 5, (result == true)&&(getDependencies().contains(new Pair<Neuron, Integer>(dependency, weight))))
+     * @post | getDependencies().size() == old(getDependencies().size()) + (result ? 1 : 0)
+     * */
+
 
     public boolean connect(Neuron dependency, int weight)
     {
-        return true;
+    	if (dependencies.size() == 5)
+    	{
+    		return false;
+		}
+    	else
+    	{
+            var pair = new Pair<Neuron, Integer>(dependency, weight);
+            dependencies.add(pair);
+            return true;
+    	}
     }
 
     public void setBias(int bias)
