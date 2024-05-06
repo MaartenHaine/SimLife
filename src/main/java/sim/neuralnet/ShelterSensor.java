a692b8bf8d84f8b045a9483991547b0a17cd589d
Maarten <r0896757@kuleuven.be>
2024-05-06 22:28:11 +0200

    Added doc for shelterSensor and added Sim tests

--- src/main/java/sim/neuralnet/ShelterSensor.java
+++ src/main/java/sim/neuralnet/ShelterSensor.java
@@ -2,6 +2,14 @@ package sim.neuralnet;
 
 import sim.entities.Prey;
 
+/**
+ * @immutable
+ * 
+ * @invar | prey != null
+ * @post check if orientation of the pray is the same as the shortest one to the shelter 
+ * | prey.getOrientation().isEqual(prey.getPosition().vectorTo(prey.getShelter().getPosition()).toClosestOrientation())
+ * 
+ */
 public class ShelterSensor extends BinarySensorNeuron
 {
 	@Override
