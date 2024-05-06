e05ab7f6731086a79301fc41c1e465623ac26fe2
Maarten <r0896757@kuleuven.be>
2024-05-06 22:32:32 +0200

    weird error in prey != null?

--- src/main/java/sim/neuralnet/ShelterSensor.java
+++ src/main/java/sim/neuralnet/ShelterSensor.java
@@ -8,7 +8,6 @@ import sim.entities.Prey;
  * @invar | prey != null
  * @post check if orientation of the pray is the same as the shortest one to the shelter 
  * | prey.getOrientation().isEqual(prey.getPosition().vectorTo(prey.getShelter().getPosition()).toClosestOrientation())
- * 
  */
 public class ShelterSensor extends BinarySensorNeuron
 {
