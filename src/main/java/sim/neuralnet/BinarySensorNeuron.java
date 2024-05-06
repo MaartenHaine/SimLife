27db62c8d7333b6a4f236e9b0da9a9054a4e4a7b
Maarten <r0896757@kuleuven.be>
2024-05-06 22:31:07 +0200

    fixed doc binary

--- src/main/java/sim/neuralnet/BinarySensorNeuron.java
+++ src/main/java/sim/neuralnet/BinarySensorNeuron.java
@@ -6,8 +6,8 @@ import sim.entities.Prey;
  * @immutable
  * 
  * @invar | prey != null
- * @post | @implies(detect(prey) == true, 750) 
- * @post | @implies(detect(prey) == false, -750)
+ * @post | implies(detect(prey) == true, 750) 
+ * @post | implies(detect(prey) == false, -750)
  */
 public abstract class BinarySensorNeuron extends SensorNeuron
 {
